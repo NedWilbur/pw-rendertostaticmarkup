ee948a181652451a202ae17c8d8c65fc6a54bfec
Ned Wilbur <Ned612@Gmail.com>
2025-04-17 10:47:38 -0400

    Testing w/ standard PW libs, no CT libs. Same issue occurs.

---
+++ playwright.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./src",
+});
